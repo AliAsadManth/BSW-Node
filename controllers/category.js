3815ad5c2729ec19793f81f687116247a46694ad
Najam Ul Saqib <91633381+1njm@users.noreply.github.com>
2022-01-04 22:42:13 +0500

    Sub cat bug fixed

--- controllers/category.js
+++ controllers/category.js
@@ -5,115 +5,120 @@ const Product = db.Product;
 
 //? Category Methods
 async function getAllCategory(req, res) {
-    const categories = await Category.find();
-    res.json(categories);
+  const categories = await Category.find();
+  res.json(categories);
 }
 async function createCategory(req, res) {
-    try {
-        if(req.body.parentCategory === undefined){
-            res.status(200).json({msg: 'Please Select Category'});
-            return;
-        }
-        await Category.create(req.body).then(()=>{
-            res.status(200).json({msg: "Category Created Sucessfully"});
-        });
-    } catch (err) {
-        res.status(500).json(err);
+  try {
+    if (req.body.parentCategory === undefined) {
+      res.status(200).json({ msg: "Please Select Category" });
+      return;
     }
+    await Category.create(req.body).then(() => {
+      res.status(200).json({ msg: "Category Created Sucessfully" });
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 }
 async function deleteCategory(req, res) {
-    try {
-        let subcat = await SubCategory.findOne({parentCategory: req.params.id});
-        if(subcat){
-            res.status(200).json({msg: "Delete Sub Categories First!"});
-            return;
-        } else {
-            await Category.findByIdAndDelete(req.params.id).then(() => {
-                res.status(200).json({msg: "Category Deleted!"});
-            });
-        }
-    } catch (err) {
-        res.status(500).json(err);
+  try {
+    let subcat = await SubCategory.findOne({ parentCategory: req.params.id });
+    if (subcat) {
+      res.status(200).json({ msg: "Delete Sub Categories First!" });
+      return;
+    } else {
+      await Category.findByIdAndDelete(req.params.id).then(() => {
+        res.status(200).json({ msg: "Category Deleted!" });
+      });
     }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 }
 async function updateCategory(req, res) {
-    try {
-        await Category.findByIdAndUpdate(req.params.id, req.body).then(()=>{
-            res.status(200).json({msg: "Category Updated"});
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
+  try {
+    await Category.findByIdAndUpdate(req.params.id, req.body).then(() => {
+      res.status(200).json({ msg: "Category Updated" });
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 }
 async function getCategoryById(req, res) {
-    try {
-        let categories = await Category.find({parentCategory: req.params.id});
-        res.status(200).json(categories);
-    } catch (err) {
-        res.status(500).json(err);
-    }
+  try {
+    let categories = await Category.find({ parentCategory: req.params.id });
+    res.status(200).json(categories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 }
 
 //? Sub Category Methods
 async function getAllSubCategory(req, res) {
-    const subCategories = await SubCategory.find().populate('parentCategory', 'name parentCategory');
-    res.json(subCategories);
+  const subCategories = await SubCategory.find().populate(
+    "parentCategory",
+    "name parentCategory"
+  );
+  res.json(subCategories);
 }
 async function createSubCategory(req, res) {
-    try {
-        if(req.body.parentCategory === undefined){
-            res.status(200).json({msg: 'Please Select Category'});
-            return;
-        }
-        await SubCategory.create(req.body).then(()=>{
-            res.status(200).json({msg: "Category Created Sucessfully"});
-        });
-    } catch (err) {
-        res.status(500).json(err);
+  try {
+    if (req.body.parentCategory === undefined) {
+      res.status(200).json({ msg: "Please Select Category" });
+      return;
     }
+    await SubCategory.create(req.body).then(() => {
+      res.status(200).json({ msg: "SubCategory Created Sucessfully" });
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 }
 async function deleteSubCategory(req, res) {
-    try {
-       let ProductChk = await Product.findOne({ categoryId: req.params.id });
-      if (ProductChk) {
-        res.status(200).json({msg: "Delete Products in this category First!" });
-        return;
-      } else {
-        await SubCategory.findByIdAndDelete(req.params.id).then(() => {
-          res.status(200).json({msg: "Category Deleted!" });
-        });
-      }
-    } catch (err) {
-        res.status(500).json(err);
+  try {
+    let ProductChk = await Product.findOne({ categoryId: req.params.id });
+    if (ProductChk) {
+      res.status(200).json({ msg: "Delete Products in this category First!" });
+      return;
+    } else {
+      await SubCategory.findByIdAndDelete(req.params.id).then(() => {
+        res.status(200).json({ msg: "SubCategory Deleted!" });
+      });
     }
+  } catch (err) {
+    res.status(500).json(err);
+  }
 }
 async function updateSubCategory(req, res) {
-    try {
-        await Category.findByIdAndUpdate(req.params.id, req.body).then(()=>{
-            res.status(200).json({msg: "Category Updated"});
-        });
-    } catch (err) {
-        res.status(500).json(err);
-    }
+  try {
+    await SubCategory.findByIdAndUpdate(req.params.id, req.body).then(() => {
+      res.json({ msg: "SubCategory Updated" });
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
 }
 async function getSubcategoryById(req, res) {
-    try {
-        let subcategories = await SubCategory.find({parentCategory: req.params.id}).populate("parentCategory", "name");
-        res.status(200).json(subcategories);
-    } catch (err) {
-        res.status(500).json(err);
-    }
+  try {
+    let subcategories = await SubCategory.find({
+      parentCategory: req.params.id,
+    }).populate("parentCategory", "name");
+    res.status(200).json(subcategories);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 }
 module.exports = {
-    getAllCategory,
-    createCategory,
-    deleteCategory,
-    updateCategory,
-    getCategoryById,
+  getAllCategory,
+  createCategory,
+  deleteCategory,
+  updateCategory,
+  getCategoryById,
 
-    createSubCategory,
-    getAllSubCategory,
-    deleteSubCategory,
-    updateSubCategory,
-    getSubcategoryById,
-}
+  createSubCategory,
+  getAllSubCategory,
+  deleteSubCategory,
+  updateSubCategory,
+  getSubcategoryById,
+};

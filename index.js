4643ce971f59fab92064a624ddbf26722e7cd6c1
1njm <njm.manth@gmail.com>
2022-01-10 00:45:16 +0500

    image bug

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-
+const path = require('path');
 //? Initializing app and port
 const app = express();
 const port = process.env.PORT;
@@ -15,9 +15,10 @@ require("./utils/db");
 app.use("/", require("./routes/routes"));
 
 //! Static Image and Slider folders
-app.use("/uploads/images", express.static("./Uploads/images"));
-app.use("/uploads/pdf", express.static("./Uploads/pdf"));
-app.use("/uploads/sliders", express.static("./Uploads/sliders"));
+app.use(express.static(path.join(__dirname, 'uploads')));
+// app.use("/uploads/images", express.static("./Uploads/images"));
+// app.use("/uploads/pdf", express.static("./Uploads/pdf"));
+// app.use("/uploads/sliders", express.static("./Uploads/sliders"));
 
 //? listening to server
 app.listen(port, () => console.log("Server is running on port: " + port));

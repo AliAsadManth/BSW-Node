a196798dbc676dd3227f54c004bddd8466430a6f
1njm <njm.manth@gmail.com>
2022-01-10 23:02:49 +0500

    return url

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 require("dotenv").config();
 const express = require("express");
-const path = require('path');
+const path = require("path");
 //? Initializing app and port
 const app = express();
 const port = process.env.PORT;
@@ -18,6 +18,12 @@ app.use("/", require("./routes/routes"));
 app.use("/uploads/images", express.static("./uploads/images"));
 app.use("/uploads/pdf", express.static("./uploads/pdf"));
 app.use("/uploads/sliders", express.static("./uploads/sliders"));
+
+app.use("/", (req, res) => {
+  res.send(
+    `<script>window.location.href='${process.env.RETURN_URL}';</script>`
+  );
+});
 
 //? listening to server
 app.listen(port, () => console.log("Server is running on port: " + port));
